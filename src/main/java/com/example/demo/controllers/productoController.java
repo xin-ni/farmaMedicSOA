30911de5e7520c35286f7989e5532a9cca287b12
Estefany Roman <estefanyromanbroncano@gmail.com>
2023-07-23 18:57:55 -0500

    productos modal, falta el editar

--- src/main/java/com/example/demo/controllers/productoController.java
+++ src/main/java/com/example/demo/controllers/productoController.java
@@ -1,11 +1,10 @@
 package com.example.demo.controllers;
 
-
 import com.example.demo.models.productoModel;
 import com.example.demo.services.categoriaService;
 import com.example.demo.services.productoService;
 import com.example.demo.models.categoriaModel;
-import java.util.List; 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,7 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
@@ -23,7 +22,6 @@ import org.springframework.beans.propertyeditors.CustomDateEditor;
 public class productoController {
 
     @Autowired
-    
 
     private final productoService productoService;
     private final categoriaService categoriaService;
@@ -33,32 +31,35 @@ public class productoController {
         this.productoService = productoService;
         this.categoriaService = categoriaService;
     }
+
     @InitBinder
     public void initBinder(WebDataBinder binder) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
     }
-    
 
     @GetMapping("/")
     public String listarProductos(Model model) {
         model.addAttribute("productos", productoService.obtenerProductos());
+        model.addAttribute("nuevoProducto", new productoModel());
+        List<categoriaModel> categoriasActivas = categoriaService.obtenerCategoriasActivas();
+        model.addAttribute("categorias", categoriasActivas);
         return "listaProductos";
     }
+
     @GetMapping("/crear")
     public String mostrarFormularioCreacion(Model model) {
         model.addAttribute("producto", new productoModel());
-        List<categoriaModel> categoriasActivas = categoriaService.obtenerCategoriasActivas();
-        model.addAttribute("categorias", categoriasActivas);
+
         return "formularioCreacionProducto";
     }
-    
 
     @PostMapping("/crear")
     public String crearProducto(@ModelAttribute productoModel producto) {
         productoService.guardarProducto(producto);
         return "redirect:/entity/productos/";
     }
+
     @GetMapping("/editar/{id}")
     public String mostrarFormularioEdicion(@PathVariable int id, Model model) {
         model.addAttribute("producto", productoService.obtenerProductoPorId(id).orElse(null));
@@ -66,8 +67,6 @@ public class productoController {
         model.addAttribute("categorias", categoriasActivas);
         return "formularioEdicionProducto";
     }
-    
-    
 
     @PostMapping("/editar/{id}")
     public String editarProducto(@PathVariable int id, @ModelAttribute productoModel producto) {
@@ -82,4 +81,3 @@ public class productoController {
         return "redirect:/entity/productos/";
     }
 }
-
