cfdf6cc315859382978ed94de40976a1abc8c67c
Estefany Roman <estefanyromanbroncano@gmail.com>
2023-07-23 16:34:30 -0500

    pedidos

---
+++ src/main/java/com/example/demo/models/pedido.java
@@ -0,0 +1,32 @@
+package com.example.demo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="pedido")
+@Getter
+@Setter
+@ToString
+public class pedido {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private int idPedido;
+    //realizar una llave secundaria
+    @ManyToOne
+    @JoinColumn(name = "idProducto")
+    private productoModel producto;
+    //se llama a la clase
+    private int cantidad;
+    private double precioCompra;
+}
