e480904747d0a5afab5f9a8a834a812920a0673a
Estefany Roman <estefanyromanbroncano@gmail.com>
2023-07-24 03:35:08 -0500

    arreglado

--- src/main/java/com/example/demo/controllers/productoController.java
+++ src/main/java/com/example/demo/controllers/productoController.java
@@ -1,19 +1,21 @@
 package com.example.demo.controllers;
 
+
 import com.example.demo.models.productoModel;
 import com.example.demo.services.categoriaService;
 import com.example.demo.services.productoService;
-import com.example.demo.models.categoriaModel;
+
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 @RequestMapping("/entity/productos")
@@ -21,8 +23,6 @@ public class productoController {
 
     @Autowired
     private productoService productoService;
-
-    @Autowired
     private categoriaService categoriaService;
     @GetMapping("/")
     public String listarProductos(Model model) {
@@ -73,21 +73,16 @@ public class productoController {
         return "redirect:/entity/productos/";
     }
 
-
-
-    //vista vendedor
-
-@GetMapping("/productoStock")
-    public String listarProductosVendedor(Model model) {
+    @GetMapping("/productoVendedor")
+    public String listarProductoVendedor(Model model) {
         List<productoModel> productos = productoService.obtenerProductos();
-
-        // Ordenar la lista de productos por stock (de menor a mayor)
-        Collections.sort(productos, Comparator.comparingInt(productoModel::getStock));
-
-        model.addAttribute("productos", productos);
         model.addAttribute("nuevoProducto", new productoModel());
-        List<categoriaModel> categoriasActivas = categoriaService.obtenerCategoriasActivas();
-        model.addAttribute("categorias", categoriasActivas);
+     Collections.sort(productos, Comparator.comparingInt(productoModel::getStock));
+        // Agregar la lista de categorías al modelo
+        model.addAttribute("categorias", categoriaService.obtenerCategorias());
+    
         return "listaProductosVendedor";
     }
+
+
 }
